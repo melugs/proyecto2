8d1092c84064bdf57843b8a4ffb9b5b31688b08b
melugs <melu9313@gmail.com>
2023-02-28 01:43:52 -0300

    task 2

---
+++ scripts/ucEvents.js
@@ -0,0 +1,10 @@
+const eventos = document.querySelector("#contenedor")
+
+let tarjetas = ''
+
+for (const evento of data.events) {
+    if (evento.date > data.currentDate)
+    tarjetas += `<div class="card"><img src="${evento.image} class="card-img-top" alt"img"><div class="card-body"><h5 class="card-title">${evento.name}</h5><a href="./details.html" class="btn btn-outline-dark">Details</a></div></div>`
+}
+
+eventos.innerHTML = tarjetas
